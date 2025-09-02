2ffc0d15fb2a862d1eff394c07c5853e7b175a37
dakshgoel2008 <goel.daksh2008@gmail.com>
2025-09-02 11:05:01 +0530

    fix:deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        outDir: path.resolve(__dirname, "../dist"),
+    },
+});
